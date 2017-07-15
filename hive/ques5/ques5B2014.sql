beee8508490ef0820e0fd0499cb5d52751d4b87d
hduser <kkchaudhary11@gmail.om>
2017-07-15 12:04:27 -0700

    adding mapreduce

--- hive/ques5/ques5B2014.sql
+++ hive/ques5/ques5B2014.sql
@@ -1,1 +1,1 @@
-select jobtile,year,count(jobtile),case_status as count from h1b.h1b_final where year=2014 and case_status =='CERTIFIED' group by jobtile,year,case_status order by count desc limit 10;
+select job_title,year,count(job_title),case_status as count from h1b.h1b_final where year=2014 and case_status =='CERTIFIED' group by job_title,year,case_status order by count desc limit 10;
